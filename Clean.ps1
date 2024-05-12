1e7107f316f97fd281e68eac0e9bc13e29e4f8c5
Matthew Elgert <mdelgert@yahoo.com>
2024-05-12 00:11:57 -0700

    add example debloat scripts

---
+++ Clean.ps1
@@ -0,0 +1,30 @@
+# Define the package names to be uninstalled
+$packageNames = @(
+    "Clipchamp",
+    "Cortana",
+    "Microsoft News",
+    "MSN Weather",
+    "Xbox",
+    "Office",
+    "Microsoft People",
+    "Microsoft Sticky Notes",
+    "Windows Maps",
+    "Xbox TCUI",
+    "Xbox Game Bar Plugin",
+    "Xbox Game Bar",
+    "Xbox Identity Provider",
+    "Xbox Game Speech Window",
+    "Your Phone",
+    "Mail and Calendar",
+    "Microsoft Teams",
+    "Power Automate",
+    "Microsoft To Do",
+    "Movies & TV",
+    "Microsoft OneDrive"
+)
+
+foreach ($packageName in $packageNames) {
+    # Uninstall the package without confirmation
+    #winget uninstall --id $packageName
+    winget uninstall $packageName --accept-source-agreements --disable-interactivity
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Win11
 Windows 11 Scripts
+
+# Examples
+https://github.com/Raphire/Win11Debloat
+
+# Links
+https://github.com/Microsoft/vscode/issues/42889
+https://stackoverflow.com/questions/75273110/add-open-with-visual-studio-code-shortcut-to-right-click-menu
+https://apps.microsoft.com/detail/9nblggh4nns1?rtc=1&hl=en-us&gl=US#activetab=pivot:overviewtab
+
+# Winget
+``ps
+winget list | sort
+``

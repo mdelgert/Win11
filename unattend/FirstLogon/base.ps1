4fa414fe4d53fd41b652bda7af688f45425375a9
Matthew David Elgert <mdelgert@yahoo.com>
2026-03-05 01:36:31 -0700

    Add wait loop for winget availability before installation

--- unattend/FirstLogon/base.ps1
+++ unattend/FirstLogon/base.ps1
@@ -6,6 +6,16 @@ $autologon = "$env:LOCALAPPDATA\Microsoft\WinGet\Packages\Microsoft.Sysinternals
 $repoSource = "https://github.com/mdelgert/Win11.git"
 $repoDestination = "C:\source\win11"
 
+# Wait for winget to become available
+$timeout = [datetime]::Now.AddMinutes(5)
+while (-not (Test-Path $winget)) {
+    if ([datetime]::Now -gt $timeout) {
+        throw "winget was not found within 5 minutes."
+    }
+    Write-Host "Waiting for winget to become available..."
+    Start-Sleep 1
+}
+
 # Install Git
 & $winget install Git.Git `
     --accept-package-agreements `
